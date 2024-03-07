467e47c56401f9d55e1700e3df926cb536a8c17f
Anthony Rosales <anthony.rosales1@pcc.edu>
2024-03-07 14:51:27 -0800

    Lab 4 progress

---
+++ PasswordManagerWeb.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request
+from AccountList import AccountList
+
+
+class PasswordManagerWeb:
+    __app = Flask(__name__)
+    __account_lists = None
+    __all_accounts = None
+
+    def __init__(self):
+        PasswordManagerWeb.__app.secret_key = "some string value"
+
+    @staticmethod
+    def read_account_lists():
+        return AccountList.get_account_lists()
+    #read - have readaccountlist in passswordmanager but cant import?
+    #Failure error not found when running print_account_list in web browswer?
+
+    @staticmethod
+    @__app.route("/print_account_lists")
+    def print_account_lists():
+        return render_template("print_account_lists.html", account_lists=PasswordManagerWeb.__account_lists)
+
+    @staticmethod
+    @__app.route("/select_account_list_for_print")
+    def select_account_list_for_print():
+        return render_template(
+            "select_account_list_for_print.html",
+            account_lists=PasswordManagerWeb.__account_lists
+        )
+
+    @staticmethod
+    @__app.route("/print_account_list")
+    def print_account_list():
+        account_list_name = request.args["account_list_name"]
+        for account_list in PasswordManagerWeb.__account_lists:
+            if account_list.get_key() == account_list_name.lower():
+                return render_template("print_account_list.html", account_list=account_list)
+        return "<h1>Error! Couldn't find an account list named " + account_list_name + "!</h1>"
+
+    def run(self):
+        PasswordManagerWeb.__account_lists, PasswordManagerWeb.__all_accounts = PasswordManagerWeb.read_account_lists()
+        PasswordManagerWeb.__app.run(port=8000)
+
+
+if __name__ == "__main__":
+    app = PasswordManagerWeb()
+    app.run()
