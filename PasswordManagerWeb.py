1c0d7d78fe57f734668f936f78f353e4c8bc2943
Anthony Rosales <anthony.rosales1@pcc.edu>
2024-03-08 18:52:03 -0800

    HTML CCS3 Lab 4 progress

--- PasswordManagerWeb.py
+++ PasswordManagerWeb.py
@@ -4,7 +4,7 @@ from AccountList import AccountList
 
 class PasswordManagerWeb:
     __app = Flask(__name__)
-    __account_lists = None
+    __all_account_lists = None
     __all_accounts = None
 
     def __init__(self):
@@ -19,27 +19,30 @@ class PasswordManagerWeb:
     @staticmethod
     @__app.route("/print_account_lists")
     def print_account_lists():
-        return render_template("print_account_lists.html", account_lists=PasswordManagerWeb.__account_lists)
+        return render_template("print_account_lists.html", account_lists=PasswordManagerWeb.__all_account_lists)
 
     @staticmethod
     @__app.route("/select_account_list_for_print")
     def select_account_list_for_print():
         return render_template(
             "select_account_list_for_print.html",
-            account_lists=PasswordManagerWeb.__account_lists
+            account_lists=PasswordManagerWeb.__all_account_lists
         )
 
     @staticmethod
     @__app.route("/print_account_list")
     def print_account_list():
-        account_list_name = request.args["account_list_name"]
-        for account_list in PasswordManagerWeb.__account_lists:
+        if "account_list" in request.args:
+            account_list_name = request.args["account_list"]
+        else:
+            account_list_name = AccountList.ALL_ACCOUNTS_NAME
+        for account_list in PasswordManagerWeb.__all_account_lists:
             if account_list.get_key() == account_list_name.lower():
                 return render_template("print_account_list.html", account_list=account_list)
         return "<h1>Error! Couldn't find an account list named " + account_list_name + "!</h1>"
 
     def run(self):
-        PasswordManagerWeb.__account_lists, PasswordManagerWeb.__all_accounts = PasswordManagerWeb.read_account_lists()
+        PasswordManagerWeb.__all_accounts, PasswordManagerWeb.__all_account_lists = AccountList.read_data()
         PasswordManagerWeb.__app.run(port=8000)
 
 
